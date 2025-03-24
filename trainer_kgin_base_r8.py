78d60823e98d8656b2b261982dd3ee4a500f68c4
itzzy <549926988@qq.com>
2025-03-24 10:31:24 +0800

    trainer_kgin_base_r8.py

--- trainer_kgin_base_r8.py
+++ trainer_kgin_base_r8.py
@@ -255,7 +255,7 @@ class TrainerKInterpolator(TrainerAbstract):
             psnr_mean = np.mean(psnr_values)
             psnr_var = np.var(psnr_values)
             # 打印结果
-            print(f'\nkgin_base_r10 Validation PSNR - Mean: {psnr_mean:.4f} ± {np.sqrt(psnr_var):.4f} | Variance: {psnr_var:.4f}')
+            print(f'\nkgin_base_r8 Validation PSNR - Mean: {psnr_mean:.4f} ± {np.sqrt(psnr_var):.4f} | Variance: {psnr_var:.4f}')
             
             print('...', out.shape, out.dtype)
             out = out.cpu().data.numpy()
@@ -265,7 +265,7 @@ class TrainerKInterpolator(TrainerAbstract):
             # np.save('out_kgin_base_0108.npy', out)
             # 尝试保存数组到文件，如果文件已存在则覆盖
             try:
-                np.save('out_kgin_base_r8_0321.npy', out)
+                np.save('out_kgin_base_0324_r8.npy', out)
             except OSError as e:
                 print(f"An error occurred: {e}")
             self.logger.update_best_eval_results(self.logger.get_metric_value('val/psnr'))
